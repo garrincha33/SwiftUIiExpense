c794f709d98344359bf367bd1903798124ad168e
Richard Price <richyprice77@gmail.com>
2022-02-02 18:39:21 +0000

    inital commit

---
+++ SwiftUIiExpense/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIiExpense
+//
+//  Created by Richard Price on 02/02/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIiExpense/SwiftUIiExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIiExpenseApp.swift
+//  SwiftUIiExpense
+//
+//  Created by Richard Price on 02/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIiExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
