ce18bb03af96b9ec949b969cd313434f3f4764e5
Richard Price <richyprice77@gmail.com>
2022-02-03 05:09:26 +0000

    added a navigationView listing items with an add button toolbar and swipe on delete

---
+++ SwiftUIiExpense/ExpenseItems.swift
@@ -0,0 +1,14 @@
+//
+//  ExpenseItems.swift
+//  SwiftUIiExpense
+//
+//  Created by Richard Price on 03/02/2022.
+//
+
+import Foundation
+
+struct ExpenseItems {
+    let name: String
+    let type: String
+    let amount: Double
+}
